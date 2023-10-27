b66c135918170e9d89489a1baa09f5031917c0b5
Achyuth Ashok <achyuth.ashok@ukaea.uk>
2023-10-27 15:49:34 +0100

    Initial Commit

---
+++ skivy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export MARTe2_Components_DIR=/home/superuser/Projects/MARTe2-components
+export MARTe2_DIR=/home/superuser/Projects/MARTe2-dev
+export LD_LIBRARY_PATH=/home/superuser/Projects/MARTe2-dev/Build/x86-linux/Core
